Fixed slice thickness FWHM logic to only use the highest peak after searching, rather than taking an average of multiple peaks. This prevents local maxima about the background level from skewing the peak search

Scottish-Medium-ACR-Analysis-Framework/hazenlib/tasks/acr_slice_thickness_rsch.py
@@ -11,6 +11,7 @@ import cv2
 from skimage.measure import profile_line
 from scipy.ndimage import gaussian_filter1d
 from scipy.signal import find_peaks
+from scipy.interpolate import Akima1DInterpolator
 
 from hazenlib.HazenTask import HazenTask
 from hazenlib.ACRObject import ACRObject
@@ -48,38 +49,47 @@ class ACRSliceThickness(HazenTask):
             res = dcm_st.PixelSpacing  # In-plane resolution from metadata
         else:
             import hazenlib.utils
+
             res = hazenlib.utils.GetDicomTag(dcm_st, (0x28, 0x30))
 
         lines, detectedInsert = self.place_lines(dcm_st.pixel_array)
         for line in lines:
             line.analyse_profile(dcm_st.pixel_array)
         slice_thickness = round(
-            0.2 * np.mean(res) * (lines[0].FWHM * lines[1].FWHM) / (lines[0].FWHM + lines[1].FWHM), 1
+            0.2 * np.mean(res) * (lines[0].FWHM * lines[1].FWHM) / (lines[0].FWHM + lines[1].FWHM),
+            1,
         )
 
         if self.report:
-            fig, axes = plt.subplots(1, 2, figsize=(16,8))
+            fig, axes = plt.subplots(1, 3, figsize=(16, 8))
             plt.tight_layout()
-            
+
             axes[0].set_title("Line profile placement.")
-            axes[1].set_title("Line profile placement")
+            axes[1].set_title("FWHM Profile 1")
+            axes[2].set_title("FWHM Profile 2")
             axes[0].imshow(dcm_st.pixel_array)
-            axes[0].plot([j.x for j in detectedInsert] + [detectedInsert[0].x], [j.y for j in detectedInsert] + [detectedInsert[0].y])
-            
+            axes[0].plot(
+                [j.x for j in detectedInsert] + [detectedInsert[0].x],
+                [j.y for j in detectedInsert] + [detectedInsert[0].y],
+            )
+
             colors = ["g", "r"]
-            for col, line in zip(colors, lines):
-                axes[0].plot([line.start.x, line.end.x], [line.start.y, line.end.y], lw=2, color=col)
-                axes[1].plot(line.profile * np.mean(res), color=col)
-                axes[1].plot(line.binarizedProfile * np.mean(res), ls="--", color=col)
+            plotIndices = [1, 2]
+            for col, line, plotIndex in zip(colors, lines, plotIndices):
+                axes[0].plot(
+                    [line.start.x, line.end.x], [line.start.y, line.end.y], lw=2, color=col
+                )
+                axes[plotIndex].plot(line.profile * np.mean(res), color=col)
+                axes[plotIndex].plot(line.binarizedProfile * np.mean(res), ls="--", color=col)
             plt.show()
-            
-            
-            img_path = os.path.realpath(os.path.join(self.report_path, f"{self.img_desc(dcm_st)}_slice_thickness.png"))
-            fig.savefig(img_path)
+
+            img_path = os.path.realpath(
+                os.path.join(self.report_path, f"{self.img_desc(dcm_st)}_slice_thickness.png")
+            )
+            fig.savefig(img_path, bbox_inches="tight")
             self.report_files.append(img_path)
 
         return slice_thickness
-
 
     @staticmethod
     def place_lines(img):
@@ -91,23 +101,33 @@ class ACRSliceThickness(HazenTask):
         Returns:
             lines (list of Line): list of line objects representing the placed lines on the image.
         """
-        
+
         # Enhance contrast, otsu threshold and binarize
         clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(3, 3))
         img_contrastEnhanced = clahe.apply(img)
-        _ , img_binary = cv2.threshold(img_contrastEnhanced, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        
+        _, img_binary = cv2.threshold(
+            img_contrastEnhanced, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+        )
+
         # Find contour of insert
         # Hough transform seems to be a potential improvement?
-        contours, _ = cv2.findContours(img_binary.astype(np.uint8), mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-        contours = sorted(contours, key=lambda cont: abs(np.max(cont[:, 0, 0]) - np.min(cont[:, 0, 0])), reverse=True)
+        contours, _ = cv2.findContours(
+            img_binary.astype(np.uint8), mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE
+        )
+        contours = sorted(
+            contours,
+            key=lambda cont: abs(np.max(cont[:, 0, 0]) - np.min(cont[:, 0, 0])),
+            reverse=True,
+        )
         insertContour = contours[1]
         insertContour = np.intp(cv2.boxPoints(cv2.minAreaRect(insertContour)))
         insertCorners = [Point(coords) for coords in insertContour]
 
         # Offset points by 1/3 of distance to nearest point, towards that point
         testPoint = insertCorners[0]
-        _, closest, middle, furthest = sorted(insertCorners, key=lambda otherPoint: (testPoint - otherPoint).mag)
+        _, closest, middle, furthest = sorted(
+            insertCorners, key=lambda otherPoint: (testPoint - otherPoint).mag
+        )
         offset_points = [
             testPoint.copy().offset(closest, sf=1 / 3),
             closest.copy().offset(testPoint, sf=1 / 3),
@@ -117,18 +137,23 @@ class ACRSliceThickness(HazenTask):
 
         # Offset points by 1/8 of distance to line pair point, towards that point
         testPoint = offset_points[0]
-        _, closest, middle, furthest = sorted(offset_points, key=lambda otherPoint: (testPoint - otherPoint).mag)
+        _, closest, middle, furthest = sorted(
+            offset_points, key=lambda otherPoint: (testPoint - otherPoint).mag
+        )
         offset_points = [
-            testPoint.copy().offset(middle, sf=1 / 50),
-            middle.copy().offset(testPoint, sf=1 / 50),
-            closest.copy().offset(furthest, sf=1 / 50),
-            furthest.copy().offset(closest, sf=1 / 50),
+            testPoint.copy().offset(middle, sf=1 / 20),
+            middle.copy().offset(testPoint, sf=1 / 20),
+            closest.copy().offset(furthest, sf=1 / 20),
+            furthest.copy().offset(closest, sf=1 / 20),
         ]
 
         # Determine which points to join to form the lines.
         testPoint = offset_points[0]
         _, closest, middle, furthest = sorted(offset_points, key=lambda x: (testPoint - x).mag)
-        lines = [ProfileLine(start=testPoint, end=middle, referenceImg=img), ProfileLine(start=closest, end=furthest, referenceImg=img)]
+        lines = [
+            ProfileLine(start=testPoint, end=middle, referenceImg=img),
+            ProfileLine(start=closest, end=furthest, referenceImg=img),
+        ]
 
         return lines, insertCorners
 
@@ -205,12 +230,14 @@ class ProfileLine:
         self._length = np.sqrt((self.start.x - self.end.x) ** 2 + (self.start.y - self.end.y) ** 2)
 
         # Initialise profile
-        profile = profile_line(
+        self._profile = profile_line(
             image=referenceImg,
             src=self._start.xy.astype(int).tolist()[::-1],
             dst=self._end.xy.astype(int).tolist()[::-1],
         )
-        self._profile = gaussian_filter1d(profile, sigma=len(profile) / 50)
+        
+        # Old smoothing
+        # self._profile = gaussian_filter1d(profile, sigma=len(profile) / 50)
 
     @property
     def start(self):
@@ -244,7 +271,29 @@ class ProfileLine:
 
     def analyse_profile(self, img):
         """Calculate profile based on start and end points using image provided in args"""
+        background = self.determine_background(self._profile)
 
+        x_data = np.arange(len(self._profile))
+        y_data = self._profile
+
+        iterator = Akima1DInterpolator(x_data, y_data)
+        x_trend = np.linspace(x_data[0], x_data[-1], int(len(x_data)/15))
+        y_trend = iterator(x_trend)
+
+        iterator = Akima1DInterpolator(x_trend, y_trend)
+        x_smooth = np.linspace(x_data[0], x_data[-1], len(x_data))
+        y_smooth = iterator(x_smooth)
+
+        peakX, peakY = self.extract_peaks(y_smooth)
+        lim = (peakY - background)/2 + background
+        binarizedProfile = np.where(y_smooth >= lim, peakY, background)
+        FWHM = binarizedProfile.tolist().count(peakY)
+
+        self._binarizedProfile = binarizedProfile
+        self._FWHM = FWHM * self.length / len(self._profile)
+
+        """
+        OLD METHOD
         backgroundY = self.determine_background(self._profile)
         peakX, peakY = self.extract_peaks(self._profile)
         lim = (peakY - np.min(self._profile)) / 2 + np.min(self._profile)
@@ -253,8 +302,8 @@ class ProfileLine:
         FWHM = binarizedProfile.tolist().count(peakY)
 
         self._binarizedProfile = binarizedProfile
-        # Correct for distance to pixel ratio.
         self._FWHM = FWHM * self.length / len(self._profile)
+        """
 
     @staticmethod
     def determine_background(profile):
@@ -267,15 +316,17 @@ class ProfileLine:
     @staticmethod
     def extract_peaks(profile):
         peaks, properties = find_peaks(profile, prominence=3, height=0)
-        if len(peaks) == 1:
-            peakX = peaks[0]
-            peakY = properties["peak_heights"][0]
-        elif len(peaks) > 1:
-            peakX = np.mean(peaks)
-            peakY = np.mean(properties["peak_heights"])
+        if len(peaks) > 0:
+            peakIndex = np.argmax(properties["peak_heights"])
+            peakX = peaks[peakIndex]
+            peakY = profile[peakX]
         else:
             raise ValueError("No peaks detected!")
+            
         return peakX, peakY
 
     def __str__(self):
         return f"Line(\n\t{self.start},\n\t{self.end}\n)"
+    
+    
+    # I believe it is peak extraction function that doesn't work properly - need to fix!