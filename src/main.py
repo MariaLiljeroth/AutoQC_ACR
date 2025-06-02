01f9a616194b636872c7bc9352d6eda891907491
xleyn <nathanjcrossley@gmail.com>
2025-06-02 16:46:54 +0100

    Restructured frozen attr detection in main for efficiency

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from pathlib import Path
 import multiprocessing as mp
 
 # Append hazenlib parent directory to sys.path as required for imports internal to Scottish-Medium-ACR-Analysis-Framework
-main_path = sys.argv[0] if getattr(sys, "frozen", False) else __file__
+frozen = getattr(sys, "frozen", False)
+main_path = sys.argv[0] if frozen else __file__
 sys.path.append(str((Path(main_path).parent / "backend/smaaf").resolve()))
 
 from frontend.app import App
@@ -13,10 +14,8 @@ from frontend.app import App
 if __name__ == "__main__":
     mp.freeze_support()
     app = App()
-    try:
+    if frozen:
         import pyi_splash  # type: ignore
 
         pyi_splash.close()
-    except ImportError:
-        pass
     app.mainloop()
