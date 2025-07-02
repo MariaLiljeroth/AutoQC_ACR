f9ff8fd6fe315687bdd1f4fc30f98423e7e45902
xleyn <nathanjcrossley@gmail.com>
2025-07-02 12:09:54 +0100

    Improved sorting behaviour for invalid dicom files (i.e. those that aren't required for analysis, e.g. those without a series description or image data).

--- build.py
+++ build.py
@@ -1,13 +1,27 @@
+"""build.py
+
+A build script for AutoQC_ACR, which bundles the project into a distributable directory using pyinstaller.
+This script pulls the pysintaller .spec file, which details the specifics of the bundling process.
+When the script is run, the user is prompted to enter a version number for the build. The suggested version number convention
+is MAJOR.MINOR.REVISION.BUILD, as is standard. The bundled project will be available in dist/
+
+"""
+
 import subprocess
 import sys
 
+
+# Specify version number for build - MAJOR.MINOR.REVISION(.BUILD)
 version = input("Please enter version number for new build: ").strip()
 
+# Do not build if no version number entered.
 if not version:
     print("No version number entered, aborting build process.")
     sys.exit()
 
-# Can insert --clean flag into cli_args for totally fresh build
+
+# Determine command line interface args and pass to subprocess.
+# Can insert --clean flag into cli_args for totally fresh build (no cache dependency).
 cli_args = [
     "pyinstaller",
     "--noconfirm",
@@ -15,5 +29,4 @@ cli_args = [
     f"dist/AutoQC_ACR_v{version}",
     "AutoQC_ACR.spec",
 ]
-
 subprocess.run(cli_args)
--- src/backend/sort_dicoms.py
+++ src/backend/sort_dicoms.py
@@ -12,7 +12,7 @@ class DicomSorter:
         dir (Path): Directory containing DICOM files.
         uid_suffix_mapper (dict[str, str]): Dictionary mapping SeriesInstanceUID to suffixes.
             Useful for Siemens scanners with duplicate data sets (see SNR by subtraction)
-        tracked_sDescrips (list[str]): List of SeriesDescription values that have been tracked.
+        tracked_series_descs (list[str]): List of Serieseries_desction values that have been tracked.
     """
 
     TRANSLATION_TABLE = str.maketrans({c: "_" for c in '\\/:*?"<>|'})
@@ -25,33 +25,45 @@ class DicomSorter:
         """
         self.dir = dir
         self.uid_suffix_mapper = {}
-        self.tracked_sDescrips = []
+        self.tracked_series_descs = []
 
     def run(self):
         """Entry function for running DICOM sorting process"""
         self.dcms = self.get_valid_DICOMs()
         for dcm in self.dcms:
-            # get UID and SeriesDescription tags and populate trackers/mappers.
+            # Read DICOM metadata
             metadata = pydicom.dcmread(dcm)
-            uid, sDescrip = metadata.SeriesInstanceUID, metadata.SeriesDescription
 
-            # Clean up series description to make safe for file explorer paths.
-            sDescrip = sDescrip.translate(self.TRANSLATION_TABLE)
+            # Check whether all tags exist that are needed for meaningful analysis.
+            required_tags = ("SeriesInstanceUID", "Serieseries_desction", "PixelData")
+            required_tags_exist = all([hasattr(metadata, tag) for tag in required_tags])
 
-            self.populate_uid_suffix_mapper(uid, sDescrip)
+            if required_tags_exist:
+                # get uid and series description
+                uid = metadata.get("SeriesInstanceUID")
+                series_desc = metadata.get("Serieseries_desction")
 
-            # Work out target folder path, create and move DICOM there.
-            target_folder = (
-                self.dir / sDescrip / self.uid_suffix_mapper[uid]
-                if hasattr(metadata, "PixelData")
-                else self.dir / "NoImageData"
-            )
+                # clean s_descrip to make safe for file explorer paths
+                series_desc = series_desc.translate(self.TRANSLATION_TABLE)
+                self.populate_uid_suffix_mapper(uid, series_desc)
+
+                # get target folder
+                target_folder = self.dir / series_desc / self.uid_suffix_mapper[uid]
+
+            else:
+                # otherwise set target folder to arbitrary backup folder for dcms that are invalid
+                target_folder = self.dir / "dcm_tags_missing"
+
+            # make target folder in os and move dcm to new location.
             target_folder.mkdir(exist_ok=True)
             dcm.rename(target_folder / dcm.name)
+
             # Send update to queue for progress bar visuals.
             get_queue().put(
                 ("PROGRESS_BAR_UPDATE", "DICOM_SORTING", 1 / len(self.dcms) * 100)
             )
+
+        # send signal that dcm sorting event has concluded.
         get_queue().put(("TASK_COMPLETE", "DICOM_SORTING"))
 
     def get_valid_DICOMs(self) -> list[pydicom.FileDataset]:
@@ -71,7 +83,7 @@ class DicomSorter:
         get_queue().put(("TASK_COMPLETE", "DICOM_CHECKING"))
         return dcms
 
-    def populate_uid_suffix_mapper(self, uid: str, sDescrip: str):
+    def populate_uid_suffix_mapper(self, uid: str, series_desc: str):
         """Populates the dict self.uid_suffix_mapper if necessary. This dict
         maps UID values to different suffixes. This allows duplicate data sets
         for Siemens scanners to be sorted into a subdirectory within the main
@@ -79,10 +91,10 @@ class DicomSorter:
 
         Args:
             uid (str): SeriesInstanceUID DICOM tag
-            sDescrip (str): SeriesDescription DICOM tag
+            series_desc (str): Serieseries_desction DICOM tag
         """
-        if sDescrip not in self.tracked_sDescrips:
-            self.tracked_sDescrips.append(sDescrip)
+        if series_desc not in self.tracked_series_descs:
+            self.tracked_series_descs.append(series_desc)
             self.uid_suffix_mapper[uid] = ""
         else:
             if uid not in self.uid_suffix_mapper:
