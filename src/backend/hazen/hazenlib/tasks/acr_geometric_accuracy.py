939254a5f07c9c045444bd75e55e0140da443f08
xleyn <nathanjcrossley@gmail.com>
2025-07-04 15:57:52 +0100

    Documentation finished for geometric_accuracy.py

--- src/backend/hazen/hazenlib/tasks/acr_geometric_accuracy.py
+++ src/backend/hazen/hazenlib/tasks/acr_geometric_accuracy.py
@@ -1,22 +1,18 @@
 """
-ACR Geometric Accuracy
+acr_geometric_accuracy
 
 https://www.acraccreditation.org/-/media/acraccreditation/documents/mri/largephantomguidance.pdf
 
-Calculates geometric accuracy for slices 1 and 5 of the ACR phantom.
-
-This script calculates the horizontal and vertical lengths of the ACR phantom in Slice 1 in accordance with the ACR Guidance.
-This script calculates the horizontal, vertical and diagonal lengths of the ACR phantom in Slice 5 in accordance with the ACR Guidance.
-The average distance measurement error, maximum distance measurement error and coefficient of variation of all distance
-measurements is reported as recommended by IPEM Report 112, "Quality Control and Artefacts in Magnetic Resonance Imaging".
-
-This is done by first producing a binary mask for each respective slice. Line profiles are drawn with aid of rotation
-matrices around the centre of the test object to determine each respective length. The results are also visualised.
+Calculates geometric accuracy for most uniform slice of ACR phantom. The horizontal, vertical and diagonal lengths
+are calculated from the mask of the most uniform slice. Rotation matrices around the mask centre are used to determine
+diagonal lengths. The results are also visualised.
 
 Created by Yassine Azma
 yassine.azma@rmh.nhs.uk
-
 18/11/2022
+
+Adapted by Nathan Crossley for local RSCH purposes, 2025
+
 """
 
 import os
@@ -31,9 +27,8 @@ from backend.hazen.hazenlib.masking_tools.slice_mask import SliceMask
 
 
 class ACRGeometricAccuracy(HazenTask):
-    """Geometric accuracy measurement class for DICOM images of the ACR phantom
-
-    Inherits from HazenTask class
+    """Subclass of HazenTask that contains code relating to calculating
+    the geometric accuracy of MR dcms.
     """
 
     def __init__(self, **kwargs):
@@ -44,11 +39,15 @@ class ACRGeometricAccuracy(HazenTask):
         self.ACR_obj = ACRObject(self.dcm_list)
 
     def run(self) -> dict:
-        """Main function for performing geometric accuracy measurement
-        using the fifth slice from the ACR phantom image set
+        """Entry function for performing geometric accuracy measurement
+        using the most uniform image from the ACR phantom image set
 
         Returns:
-            dict: results are returned in a standardised dictionary structure specifying the task name, input DICOM Series Description + SeriesNumber + InstanceNumber, task measurement key-value pairs, optionally path to the generated images for visualisation
+            dict: results are returned in a standardised dictionary
+                structure specifying the task name, input DICOM Series
+                Description + SeriesNumber + InstanceNumber, task
+                measurement key-value pairs, optionally path to the
+                generated images for visualisation.
         """
 
         # Identify relevant slice and get dicom and mask
@@ -56,21 +55,28 @@ class ACRGeometricAccuracy(HazenTask):
         dcm_geom = self.ACR_obj.dcms[target_slice]
         mask_geom = self.ACR_obj.masks[target_slice]
 
-        # Initialise results dictionary
+        # Initialise results dictionary and add image description
         results = self.init_result_dict()
         results["file"] = self.img_desc(dcm_geom)
 
         try:
-            lengths_5 = self.get_geometric_accuracy_slice5(dcm_geom, mask_geom)
+            # calculate geometric accuracy using chosen dcm and mask
+            lengths_5 = self.get_geometric_accuracy(dcm_geom, mask_geom)
+
+            # append the four calculated lengths to the results dict
             results["measurement"] = {
                 "Horizontal distance": round(lengths_5[0], 2),
                 "Vertical distance": round(lengths_5[1], 2),
                 "Diagonal distance SW": round(lengths_5[2], 2),
                 "Diagonal distance SE": round(lengths_5[3], 2),
             }
+
+            # signal to the user that the geomtric accuracy was successfully calculated for this dcm
             print(f"{self.img_desc(dcm_geom)}: Geometric accuracy calculated.")
 
         except Exception as e:
+
+            # inform the user that the geomtric accuracy could not be calculated and why
             print(
                 f"{self.img_desc(dcm_geom)}: Could not calculate geometric accuracy because of: {e}"
             )
@@ -82,19 +88,23 @@ class ACRGeometricAccuracy(HazenTask):
 
         return results
 
-    def get_geometric_accuracy_slice5(
-        self, dcm: FileDataset, mask: SliceMask
-    ) -> tuple[float]:
-        """Measure geometric accuracy for slice 5
+    def get_geometric_accuracy(self, dcm: FileDataset, mask: SliceMask) -> tuple[float]:
+        """Measure geometric accuracy for chosen dcm. This is achieved
+        through obtaining an on-axis bounding box for on-axis and rotated
+        versions of the mask of the dcm.
 
         Args:
-            dcm (FileDataset): DICOM image object
-            mask (SliceMask): Mask corresponding to chosen DICOM
+            dcm (FileDataset): dcm chosen for geometric accuracy task.
+            mask (SliceMask): Mask corresponding to chosen dcm.
 
         Returns:
             tuple[float]: horizontal and vertical distances, as well as diagonals (SW, SE)
         """
+
+        # get dcm "image"
         img = dcm.pixel_array
+
+        # get centre of mask
         cxy = mask.centre
 
         # get orthognal and diagonal lengths from mask
@@ -108,15 +118,18 @@ class ACRGeometricAccuracy(HazenTask):
             fig.set_size_inches(8, 24)
             fig.tight_layout(pad=4)
 
+            # show which dcm has been used and the detected centre
             axes[0].imshow(img)
             axes[0].scatter(cxy[0], cxy[1], c="red")
             axes[0].axis("off")
             axes[0].set_title("Centroid Location")
 
+            # show the elliptical mask used for geometric accuracy calculations
             axes[1].imshow(mask.elliptical_mask)
             axes[1].axis("off")
             axes[1].set_title("Elliptical Mask")
 
+            # on axis 2 show the image and the elliptical mask
             axes[2].imshow(img)
             axes[2].imshow(mask.elliptical_mask, alpha=0.4)
 
@@ -127,6 +140,7 @@ class ACRGeometricAccuracy(HazenTask):
                 ("Diagonal Start 2", "Diagonal End 2", "Diagonal Distance 2"),
             )
 
+            # for each pair of start and end points, plot arrows on axis 2 to symbolise measurements
             for i, (start, end, _) in enumerate(triplets):
                 axes[2].annotate(
                     "",
@@ -137,6 +151,7 @@ class ACRGeometricAccuracy(HazenTask):
                     ),
                 )
 
+            # add legend handles with colors matching those of arrows and labels corresponding to measured geometric accuracy values
             legend_handles = [
                 mpatches.Patch(
                     color=f"C{i}", label=f"{np.round(length_dict[dist], 2)} mm"
@@ -144,16 +159,19 @@ class ACRGeometricAccuracy(HazenTask):
                 for i, (_, _, dist) in enumerate(triplets)
             ]
 
+            # configure axis 2
             axes[2].legend(handles=legend_handles, loc="upper right", title="Distances")
-
             axes[2].axis("off")
             axes[2].set_title("Geometric Accuracy for Slice 5")
 
+            # Work out path to save plots at
             img_path = os.path.realpath(
                 os.path.join(
                     self.report_path, f"{self.img_desc(dcm)}_geom_accuracy.png"
                 )
             )
+
+            # save plots and save paths
             fig.savefig(img_path, dpi=300)
             self.report_files.append(img_path)
             plt.close()
@@ -166,24 +184,29 @@ class ACRGeometricAccuracy(HazenTask):
         )
 
     def measure_orthogonal_lengths(self, mask: SliceMask) -> dict:
-        """Measures orthogonal lengths for a mask.
+        """Measures orthogonal lengths for a given dcm mask.
+        This is achieved by taking an on-axis bounding box about
+        the mask's elliptical mask.
 
         Args:
-            mask (SliceMask): Mask to find orthogonal lengths of.
+            mask (SliceMask): Dcm mask chosen for geometric accuracy calculations.
 
         Returns:
             dict: Dictionary containing horizontal and vertical start points, end points and distances.
         """
+
+        # work out pixel spacing in mm for x and y
         dx, dy = self.ACR_obj.pixel_spacing
 
         # get on axis bounding box around mask
         x, y, w, h = cv2.boundingRect(mask.elliptical_mask)
 
-        # calculate start point, end point and distance for horizontal and vertical orientations
+        # calculate start point, end point and distance for horizontal orientation
         horizontal_start = (x, mask.centre[1])
         horizontal_end = (x + w, mask.centre[1])
         horizontal_distance = w * dx
 
+        # calculate start point, end point and distance for vertical orientation
         vertical_start = (mask.centre[0], y)
         vertical_end = (mask.centre[0], y + h)
         vertical_distance = h * dy
@@ -201,10 +224,12 @@ class ACRGeometricAccuracy(HazenTask):
         return length_dict
 
     def measure_diagonal_lengths(self, mask: SliceMask) -> dict:
-        """Measures diagonal lengths for a mask.
+        """Measures diagonal lengths for a given dcm mask.
+        This is achieved by rotating the mask by 45 degrees
+        and repeating the on-axis analysis.
 
         Args:
-            mask (SliceMask): Mask to find diagonal lengths of.
+            mask (SliceMask): Mask to use for geometric accuracy calculations
 
         Returns:
             dict: Dictionary containing the two diagonal start points, end points and distances.
@@ -215,6 +240,10 @@ class ACRGeometricAccuracy(HazenTask):
 
         # measured orthogonal distances of rotated mask (i.e. diagonal distances)
         length_dict_diag = self.measure_orthogonal_lengths(mask_45)
+
+        # adjust keys of new dict to reflect diagonal distances
+        # transform points back to un-rotated reference frame before assigning to keys
+
         key_pairs = (
             ("Diagonal Start 1", "Horizontal Start"),
             ("Diagonal End 1", "Horizontal End"),
@@ -224,8 +253,6 @@ class ACRGeometricAccuracy(HazenTask):
             ("Diagonal Distance 2", "Vertical Distance"),
         )
 
-        # adjust keys of new dict to reflect diagonal distances
-        # transform points back to un-rotated reference frame before assigning to keys
         for new_key, old_key in key_pairs:
             val = length_dict_diag.pop(old_key)
             length_dict_diag[new_key] = (
@@ -235,39 +262,3 @@ class ACRGeometricAccuracy(HazenTask):
             )
 
         return length_dict_diag
-
-    # @staticmethod
-    # def distortion_metric(L):
-    #     """Calculate the distortion metric based on length
-
-    #     Args:
-    #         L (tuple): horizontal and vertical distances from slices 1 and 5
-
-    #     Returns:
-    #         tuple of floats: mean_err, max_err, cov_l
-    #     """
-    #     err = [x - 190 for x in L]
-    #     mean_err = np.mean(err)
-
-    #     max_err = np.max(np.absolute(err))
-    #     cov_l = 100 * np.std(L) / np.mean(L)
-
-    #     return mean_err, max_err, cov_l
-
-    # @staticmethod
-    # def distortion_metric_MedPhantom(L):
-    #     """Calculate the distortion metric based on length
-
-    #     Args:
-    #         L (tuple): horizontal and vertical distances from slices 1 and 5
-
-    #     Returns:
-    #         tuple of floats: mean_err, max_err, cov_l
-    #     """
-    #     err = [x - 165 for x in L]
-    #     mean_err = np.mean(err)
-
-    #     max_err = np.max(np.absolute(err))
-    #     cov_l = 100 * np.std(L) / np.mean(L)
-
-    #     return mean_err, max_err, cov_l
